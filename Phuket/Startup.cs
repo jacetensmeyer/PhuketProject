fffc5d47f663ee71b2c7c6a073771598ff6d99e4
Katie Rios <katiebeth3@comcast.net>
2016-10-31 08:09:42 -0600

    Add project files.

---
+++ Phuket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Phuket.Startup))]
+namespace Phuket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
